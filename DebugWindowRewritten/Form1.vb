a6c1ef2f56f0a6d198889f2d7e54c84fea0911ad
Alongusernamefornothing <mylongname21@gmail.com>
2025-02-03 10:48:09 +0100

    Add files via upload

---
+++ DebugWindowRewritten/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim FormK As New CV.ProgramTester
+        FormK.Show()
+        Close()
+    End Sub
+End Class
